12fff461146bb3e516bd92254e102c85caac4d4e
Renee Zhu <zhux@nextcapital.com>
2017-06-24 16:52:11 -0500

    aa

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ DATABASE_CONNECT_OPTIONS = {}
 THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
-CSRF_ENABLED     = True
+CSRF_ENABLED     = False
 
 # Use a secure, unique and absolutely secret key for
 # signing the data.
