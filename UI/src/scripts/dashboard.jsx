afaa03afd2b5c8961c109fdd9ba507436a94ac32
Tom Patterson <tom@nextcapital.com>
2017-06-24 12:39:10 -0500

    Pull header/footer out of dash container component

--- UI/src/scripts/dashboard.jsx
+++ UI/src/scripts/dashboard.jsx
@@ -30,7 +30,36 @@ const Dashboard = React.createClass({
   },
   render() {
     return (
-      <DashboardContainer />
+      <div className="site-wrapper">
+
+        <div className="site-wrapper-inner">
+
+          <div className="cover-container">
+
+            <div className="masthead clearfix">
+              <div className="inner">
+                <h3 className="masthead-brand">Dragon Slayers</h3>
+                <nav className="nav nav-masthead">
+                  <a className="nav-link active" href="#">Vote</a>
+                  <a className="nav-link" href="#">History</a>
+                  <a className="nav-link" href="#">Other</a>
+                </nav>
+              </div>
+            </div>
+
+            <DashboardContainer />
+
+            <div className="mastfoot">
+              <div className="inner">
+                <p>Some text and a <a href="https://getbootstrap.com">link</a>.</p>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
     );
   }
 });
